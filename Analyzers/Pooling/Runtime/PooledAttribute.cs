2bb3d107ff357cb3359a8945a7194b30d4d4b952
Infinite Canvas <chanawin90@gmail.com>
2025-01-21 10:29:14 +0100

    init

---
+++ Analyzers/Pooling/Runtime/PooledAttribute.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Analyzers.Pooling.Runtime
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class PooledAttribute : Attribute
+    {
+        public string CreateFunc      { get; }
+        public string GetAction       { get; }
+        public string ReturnAction    { get; }
+        public string DestroyAction   { get; }
+        public int    DefaultCapacity { get; }
+        public int    MaxSize         { get; }
+
+        public PooledAttribute(string createFunc      = "",
+                                 string getAction       = "",
+                                 string returnAction    = "",
+                                 string destroyAction   = "",
+                                 int    defaultCapacity = 10,
+                                 int    maxSize         = 100)
+        {
+            CreateFunc = createFunc;
+            GetAction = getAction;
+            ReturnAction = returnAction;
+            DestroyAction = destroyAction;
+            DefaultCapacity = defaultCapacity;
+            MaxSize = maxSize;
+        }
+    }
+}
